0b295d673be1da39764e4b099e51eb10c92be9c7
Mogan Xumerle <moganrz@gmail.com>
2018-09-07 15:50:37 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/mogan/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.mogan.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mogan.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
